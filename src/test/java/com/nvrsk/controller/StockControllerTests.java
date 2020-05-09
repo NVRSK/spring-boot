7d56be696f161c77009f277394f241e96a5a8468
NVRSK <kunikov2140@mail.ru>
2020-05-09 15:17:06 +0400

    Updated StockControllerTests for :POST method

--- src/test/java/com/nvrsk/controller/StockControllerTests.java
+++ src/test/java/com/nvrsk/controller/StockControllerTests.java
@@ -1,7 +1,13 @@
 package com.nvrsk.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nvrsk.exception.IncorrectRequestException;
+import com.nvrsk.exception.StockAlreadyExistsException;
 import com.nvrsk.exception.StockNotFoundException;
+import com.nvrsk.model.Stock;
 import com.nvrsk.repository.StockRepository;
+import com.nvrsk.request.NewStockRequest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +16,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.time.Instant;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -61,5 +72,93 @@ class StockControllerTests {
                 .andReturn()
                 .getResolvedException();
         assertException(exception, StockNotFoundException.class, "Stock with id 3 not found");
+    }
+
+    @Test
+    void testGetUnparsedId() throws Exception {
+        Exception exception = mockMvc.perform(
+                get("/api/stocks/number"))
+                .andExpect(status().isBadRequest())
+                .andReturn()
+                .getResolvedException();
+        assertException(exception, MethodArgumentTypeMismatchException.class, "Failed to convert value of type 'java.lang.String' to required type 'long'; nested exception is java.lang.NumberFormatException: For input string: \"number\"");
+    }
+
+    @Test
+    void testPostNewStock() throws Exception {
+        NewStockRequest newStockRequest = new NewStockRequest();
+        newStockRequest.setName("   Stock3.H ");
+        newStockRequest.setPrice(0.23d);
+
+        Instant timestamp = Instant.now();
+
+        mockMvc.perform(
+                post("/api/stocks")
+                        .contentType("application/json")
+                        .content(new ObjectMapper().writeValueAsString(newStockRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(content().string("New stock was created"))
+                .andExpect(header().string("Location", equalTo("http://localhost/api/stocks/3")))
+                .andExpect(redirectedUrl("http://localhost/api/stocks/3"));
+
+        Optional<Stock> expectedStock = stockRepository.findByName("Stock3.H");
+        assertThat(expectedStock)
+                .isPresent()
+                .hasValueSatisfying(stock -> Assertions.assertAll(
+                        () -> assertThat(stock.getId()).isNotNull(),
+                        () -> assertThat(stock.getName()).isEqualTo("Stock3.H"),
+                        () -> assertThat(stock.getCurrentPrice()).isEqualTo(0.23d),
+                        () -> assertThat(stock.getLastUpdate()).isAfter(timestamp)));
+    }
+
+    @Test
+    void testPostNewStockZeroPrice() throws Exception {
+        NewStockRequest newStockRequest = new NewStockRequest();
+        newStockRequest.setName("Stock4.N");
+        newStockRequest.setPrice(0d);
+
+        Exception exception = mockMvc.perform(
+                post("/api/stocks")
+                        .contentType("application/json")
+                        .content(new ObjectMapper().writeValueAsString(newStockRequest)))
+                .andExpect(status().isBadRequest())
+                .andReturn()
+                .getResolvedException();
+
+        assertException(exception, IncorrectRequestException.class, "Stock price should be greater than 0");
+    }
+
+    @Test
+    void testPostNewStockEmptyName() throws Exception {
+        NewStockRequest newStockRequest = new NewStockRequest();
+        newStockRequest.setName("     ");
+        newStockRequest.setPrice(1.1d);
+
+        Exception exception = mockMvc.perform(
+                post("/api/stocks")
+                        .contentType("application/json")
+                        .content(new ObjectMapper().writeValueAsString(newStockRequest)))
+                .andExpect(status().isBadRequest())
+                .andReturn()
+                .getResolvedException();
+
+        assertException(exception, IncorrectRequestException.class, "Stock name can't be empty");
+    }
+
+    @Test
+    void testPostNewStockAlreadyExists() throws Exception {
+        NewStockRequest newStockRequest = new NewStockRequest();
+        newStockRequest.setName("Stock1.L");
+        newStockRequest.setPrice(1.1d);
+
+        Exception exception = mockMvc.perform(
+                post("/api/stocks")
+                        .contentType("application/json")
+                        .content(new ObjectMapper().writeValueAsString(newStockRequest)))
+                .andExpect(status().isBadRequest())
+                .andReturn()
+                .getResolvedException();
+
+        assertException(exception, StockAlreadyExistsException.class, "Stock already exists with name: Stock1.L");
     }
 }
