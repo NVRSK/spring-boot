8ca5264dff8ef7272c3df0ee3f382fcb416cfafe
NVRSK <kunikov2140@mail.ru>
2020-05-16 19:58:00 +0400

    Updated StockControllerTests, added new tests for :PUT method

--- src/test/java/com/nvrsk/controller/StockControllerTests.java
+++ src/test/java/com/nvrsk/controller/StockControllerTests.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nvrsk.exception.IncorrectRequestException;
 import com.nvrsk.exception.StockAlreadyExistsException;
 import com.nvrsk.exception.StockNotFoundException;
+import com.nvrsk.model.PriceHistory;
 import com.nvrsk.model.Stock;
 import com.nvrsk.repository.StockRepository;
 import com.nvrsk.request.NewStockRequest;
+import com.nvrsk.request.PriceUpdateRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,12 +21,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
@@ -85,6 +87,30 @@ class StockControllerTests {
     }
 
     @Test
+    void testGetHistory() throws Exception {
+        mockMvc.perform(
+                get("/api/stocks/1/history"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        "[" +
+                                "{\"price\":2.0,\"startDate\":\"2020-05-01T22:56:04Z\"}," +
+                                "{\"price\":1.99,\"startDate\":\"2020-05-01T21:54:03Z\"}" +
+                                "]",
+                        true
+                ));
+    }
+
+    @Test
+    void testGetHistoryIncorrectId() throws Exception {
+        Exception exception = mockMvc.perform(
+                get("/api/stocks/3/history"))
+                .andExpect(status().isNotFound())
+                .andReturn()
+                .getResolvedException();
+        assertException(exception, StockNotFoundException.class, "Stock with id 3 not found");
+    }
+
+    @Test
     void testPostNewStock() throws Exception {
         NewStockRequest newStockRequest = new NewStockRequest();
         newStockRequest.setName("   Stock3.H ");
@@ -105,10 +131,20 @@ class StockControllerTests {
         assertThat(expectedStock)
                 .isPresent()
                 .hasValueSatisfying(stock -> Assertions.assertAll(
-                        () -> assertThat(stock.getId()).isNotNull(),
+                        () -> assertThat(stock.getId()).isEqualTo(3),
                         () -> assertThat(stock.getName()).isEqualTo("Stock3.H"),
                         () -> assertThat(stock.getCurrentPrice()).isEqualTo(0.23d),
-                        () -> assertThat(stock.getLastUpdate()).isAfter(timestamp)));
+                        () -> assertThat(stock.getLastUpdate()).isAfter(timestamp),
+                        () -> {
+                            List<PriceHistory> history = stock.getHistory();
+                            assertThat(history).isNotNull().hasSize(1);
+                            PriceHistory priceHistory = history.get(0);
+                            Assertions.assertAll(
+                                    () -> assertThat(priceHistory.getId()).isNotNull(),
+                                    () -> assertThat(priceHistory.getPrice()).isEqualTo(0.23d),
+                                    () -> assertThat(priceHistory.getStock().getId()).isEqualTo(stock.getId()),
+                                    () -> assertThat(priceHistory.getStartDate()).isEqualTo(stock.getLastUpdate()));
+        }));
     }
 
     @Test
@@ -160,5 +196,97 @@ class StockControllerTests {
                 .getResolvedException();
 
         assertException(exception, StockAlreadyExistsException.class, "Stock already exists with name: Stock1.L");
+    }
+
+    @Test
+    void testPutNewPrice() throws Exception {
+        double newPrice = 3.1;
+
+        PriceUpdateRequest priceUpdateRequest = new PriceUpdateRequest();
+        priceUpdateRequest.setPrice(newPrice);
+
+        Optional<Stock> expectedStock = stockRepository.findById(2L);
+        assertThat(expectedStock).isPresent();
+        Stock expectedStockValue = expectedStock.get();
+        String originalStockName = expectedStockValue.getName();
+        Instant timestampBeforeUpdate = expectedStockValue.getLastUpdate();
+        double priceBeforeUpdate = expectedStockValue.getCurrentPrice();
+
+        mockMvc.perform(
+                put("/api/stocks/2")
+                        .contentType("application/json")
+                        .content(new ObjectMapper().writeValueAsString(priceUpdateRequest)))
+                .andExpect(status().isOk());
+
+        Optional<Stock> updatedStock = stockRepository.findById(2L);
+        assertThat(updatedStock)
+                .isPresent()
+                .hasValueSatisfying(stock -> Assertions.assertAll(
+                        () -> assertThat(stock.getName()).isEqualTo(originalStockName),
+                        () -> assertThat(stock.getCurrentPrice()).isEqualTo(newPrice),
+                        () -> assertThat(stock.getLastUpdate()).isAfter(timestampBeforeUpdate),
+                        () -> {
+                            List<PriceHistory> history = stock.getHistory();
+                            assertThat(history).isNotNull().hasSize(2);
+                            PriceHistory lastPriceHistory = history.get(0);
+                            Assertions.assertAll(
+                                    () -> assertThat(lastPriceHistory.getId()).isNotNull(),
+                                    () -> assertThat(lastPriceHistory.getPrice()).isEqualTo(newPrice),
+                                    () -> assertThat(lastPriceHistory.getStock().getId()).isEqualTo(stock.getId()),
+                                    () -> assertThat(lastPriceHistory.getStartDate()).isEqualTo(stock.getLastUpdate())
+                            );
+                            PriceHistory previousPriceHistory = history.get(1);
+                            Assertions.assertAll(
+                                    () -> assertThat(previousPriceHistory.getId()).isNotNull(),
+                                    () -> assertThat(previousPriceHistory.getPrice()).isEqualTo(priceBeforeUpdate),
+                                    () -> assertThat(previousPriceHistory.getStock().getId()).isEqualTo(stock.getId())
+                            );
+                        }
+                ));
+    }
+
+
+    @Test
+    void testPutNewPriceIncorrectId() throws Exception {
+        PriceUpdateRequest priceUpdateRequest = new PriceUpdateRequest();
+        priceUpdateRequest.setPrice(3d);
+
+        Exception exception = mockMvc.perform(
+                put("/api/stocks/3")
+                        .contentType("application/json")
+                        .content(new ObjectMapper().writeValueAsString(priceUpdateRequest)))
+                .andExpect(status().isNotFound())
+                .andReturn()
+                .getResolvedException();
+        assertException(exception, StockNotFoundException.class, "Stock with id 3 not found");
+    }
+
+    @Test
+    void testPutNewPriceNegative() throws Exception {
+        PriceUpdateRequest priceUpdateRequest = new PriceUpdateRequest();
+        priceUpdateRequest.setPrice(-3d);
+
+        Exception exception = mockMvc.perform(
+                put("/api/stocks/1")
+                        .contentType("application/json")
+                        .content(new ObjectMapper().writeValueAsString(priceUpdateRequest)))
+                .andExpect(status().isBadRequest())
+                .andReturn()
+                .getResolvedException();
+        assertException(exception, IncorrectRequestException.class, "Stock price should be greater than 0");
+    }
+
+    @Test
+    void testPutNewPriceNull() throws Exception {
+        PriceUpdateRequest priceUpdateRequest = new PriceUpdateRequest();
+
+        Exception exception = mockMvc.perform(
+                put("/api/stocks/1")
+                        .contentType("application/json")
+                        .content(new ObjectMapper().writeValueAsString(priceUpdateRequest)))
+                .andExpect(status().isBadRequest())
+                .andReturn()
+                .getResolvedException();
+        assertException(exception, IncorrectRequestException.class, "Stock price should be greater than 0");
     }
 }
